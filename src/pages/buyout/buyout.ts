18e9620eb037aeda79bed38219997b72c8b2a2c4
saibot777 <stefantrajkovic777@gmail.com>
2017-02-27 16:03:16 +0100

    ui ready

---
+++ src/pages/buyout/buyout.ts
@@ -0,0 +1,12 @@
+/**
+ * Created by stefan.trajkovic on 27.2.2017..
+ */
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'page-buyout',
+  templateUrl: 'buyout.html'
+})
+export class BuyoutPage {
+
+}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 
 import { NavController } from 'ionic-angular';
+import {ShopPage} from "../shop/shop";
 
 @Component({
   selector: 'page-home',
@@ -10,5 +11,9 @@ export class HomePage {
 
   constructor(public navCtrl: NavController) {
 
+  }
+
+  onGoToShop() {
+    this.navCtrl.push(ShopPage);
   }
 }
